7890b24dac5a31603ef136232a02c6d58ad67a53
tankashalarishabh <tankashalarishabh@msitprogram.net>
2020-04-13 23:32:49 +0530

    TC

--- LRU.py
+++ LRU.py
@@ -16,18 +16,35 @@ class LRU:
     def init__(self, co):
         self.co = 0
     def put(self,data):
-        if(self.co >= 4):
-
-        elif self.co == 0:
-            ll.head = Node(data);
-            self.co++;
+        if(has(data) == False):
+            if(self.co >= 4):
+                temp = head.next
+                head = temp
+                temp = head
+                for i in range(3):
+                    temp = temp.next
+                temp = Node(data)
+            elif self.co == 0:
+                ll.head = Node(data);
+                self.co++;
+            else:
+                temp = head
+                for i in range(self.co):
+                    temp = temp.next
+                temp = Node(data)
+                self.co++
         else:
-            temp = head
-            for i in range(self.co):
-                temp = temp.next
-            temp = Node(data)
-            self.co++
-            
+
+    def has(self, data):
+        if(head.data == data):
+            return True
+        temp = head.next
+        for i in range(self.co - 1):
+            if(temp.data == data):
+                return True
+            temp = temp.next
+        return False
+        
     def get_cache(self):
 
     def get(self, c):
