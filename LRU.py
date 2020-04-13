f90e110754a73f52d19afea0b0cc8aad18cb7db8
tankashalarishabh <tankashalarishabh@msitprogram.net>
2020-04-13 20:12:14 +0530

    fc

---
+++ LRU.py
@@ -0,0 +1,22 @@
+class Node: 
+    # Function to initialise the node object 
+    def __init__(self, data): 
+        self.data = data  # Assign data 
+        self.next = None  # Initialize next as null 
+  
+  
+# Linked List class contains a Node object 
+class LinkedList: 
+  
+    # Function to initialize head 
+    def __init__(self): 
+        self.head = None
+
+class LRU:
+    def init__(self, co):
+        self.co = 0;
+    def put(self,data):
+
+    def get_cache(self):
+
+    def get(self):
